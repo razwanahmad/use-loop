c65cf7c9b10bcdf6600f96216b6851d0c3356e7d
Razwan Ahmad <razwanahmad3246@gmail.com>
2026-03-25 15:23:41 +0600

    file is ready

---
+++ even/odd.py
@@ -0,0 +1,53 @@
+num=int(input("Enter a number:"))
+if num%2==0:
+    print(f'{num} is a even number')
+elif num%2!=0:
+    print(f'{num} is a odd number')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
